bd90e693ceac42177992ed7a74ce9d5a721a3401
MiniMestri <fonsild97@hotmail.com>
2023-10-19 11:53:47 +0200

    Desarrollo getchar
    Desarrollo de código donde se recoge un valor char y devuelve el valor introducido

---
+++ Fundamentos basicos C/Entradas y Salidas/EntradaSalida.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+
+int main(int argc, char *argv[]){
+    printf("Introduce un numero");
+    char num = getchar();
+    printf("El numero introducido es: %c ",num);
+    
+    return 0;
+}
