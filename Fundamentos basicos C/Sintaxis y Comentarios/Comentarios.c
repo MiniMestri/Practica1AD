3b6bab296f4889267bd53fbaaa034327bcaa2020
MiniMestri <fonsild97@hotmail.com>
2023-10-19 10:19:52 +0200

    Desarrollo de comentarios
    Desarrollo de comentarios únicos y comentarios multilinea

---
+++ Fundamentos basicos C/Sintaxis y Comentarios/Comentarios.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main(int argc, char *argv[]){
+    
+    //Comentario1
+    //Comentario2
+    
+    /*
+    Comentario
+    multilinea
+    para asignatura
+    acceso a datos
+    */
+    
+    return 0;
+}
